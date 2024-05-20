992c25ac3319f70d1b2cac97a9f0c1d49a3ef2ad
Lantz <github@muddylemon.com>
2024-05-19 20:22:04 -0500

    they not like us

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def video_to_mp3(video_path: str):
         return audio_path
 
     audio = AudioFileClip(video_path)
-    audio.write_audiofile(audio_path, logger="bar")
+    audio.write_audiofile(audio_path, logger="bar", threads=4)
     print(f"Audio saved to: {audio_path}")
     return audio_path
 
@@ -63,27 +63,25 @@ def separate_tracks(audio_file_path: str) -> tuple[str, str]:
     return f"./separated/vocals_{audio_filename}", f"./separated/music_{audio_filename}"
 
 
-def transcribe(audiofile_path: str, num_passes: int = 1, output_directory: str = "./subtitles") -> str:
+def transcribe(audiofile_path: str, num_passes: int = 1) -> str:
     """
-    Converts an MP3 file to a transcript using Whisper, assuming the last pass provides the best result.
+    Converts an MP3 file to a transcript using Whisper
 
     Args:
         audiofile_path (str): The file path of the MP3 file to be processed.
         num_passes (int): Number of transcription passes to perform.
-        output_directory (str): Directory to save the output files.
-
     Returns:
         str: The path to the SRT file containing the transcript from the last pass.
     """
     try:
-        if not os.path.exists(output_directory):
-            os.makedirs(output_directory)
+        if not os.path.exists("./subtitles"):
+            os.makedirs("./subtitles")
 
-        if os.path.exists(f"{output_directory}/{os.path.splitext(os.path.basename(audiofile_path))[0]}.srt"):
-            return f"{output_directory}/{os.path.splitext(os.path.basename(audiofile_path))[0]}.srt"
+        if os.path.exists(f"subtitles/{os.path.splitext(os.path.basename(audiofile_path))[0]}.srt"):
+            return f"subtitles/{os.path.splitext(os.path.basename(audiofile_path))[0]}.srt"
 
         device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        model = whisper.load_model("medium.en").to(device)
+        model = whisper.load_model("large-v2").to(device)
 
         last_result = None
         for i in range(num_passes):
@@ -95,13 +93,10 @@ def transcribe(audiofile_path: str, num_passes: int = 1, output_directory: str =
         if last_result is None:
             raise ValueError("No transcription results obtained.")
 
-        srt_writer = get_writer("srt", output_directory)
+        srt_writer = get_writer("srt", "./subtitles")
         srt_writer(last_result, audiofile_path, highlight_words=True)
 
-        txt_writer = get_writer("txt", output_directory)
-        txt_writer(last_result, audiofile_path)
-
-        subtitle_path = os.path.join(output_directory, os.path.splitext(
+        subtitle_path = os.path.join("./subtitles", os.path.splitext(
             os.path.basename(audiofile_path))[0] + '.srt')
         return subtitle_path
 
@@ -112,10 +107,10 @@ def transcribe(audiofile_path: str, num_passes: int = 1, output_directory: str =
 
 def create(vocals_path: str, music_path: str, video_path: str):
 
-    audio = AudioFileClip(music_path).set_fps(44100)
+    music = AudioFileClip(music_path).set_fps(44100)
     vocals_audio = AudioFileClip(vocals_path).volumex(0.05).set_fps(44100)
 
-    combined_audio = CompositeAudioClip([audio, vocals_audio])
+    combined_audio = CompositeAudioClip([music, vocals_audio])
 
     background_video = VideoFileClip(video_path, target_resolution=(720, 1280)).set_fps(
         30).set_duration(combined_audio.duration)
@@ -125,8 +120,7 @@ def create(vocals_path: str, music_path: str, video_path: str):
         fontcolor = "#aeedad" if txt == "instrumental" else "#FFEEFF"
         return TextClip(txt, font="./fonts/dv.ttf", fontsize=fontsize, color=fontcolor, stroke_color="#000000", stroke_width=0.5, size=(1240, None), method='pango')
 
-    subtitles_path = transcribe(vocals_path)
-    print(f"Subtitles path: {subtitles_path}")
+    subtitles_path = transcribe(vocals_path, 2)
 
     subtitles = SubtitlesClip(subtitles_path, generator)
 
@@ -138,7 +132,6 @@ def create(vocals_path: str, music_path: str, video_path: str):
         subtitles.set_position(('center', 'center'), relative=True)
     ]).set_audio(combined_audio)
 
-    # Save the karaoke video
     filename = f"karaoke_{os.path.basename(video_path)}"
     if not os.path.exists("./output"):
         os.makedirs("./output")
@@ -169,7 +162,7 @@ def main():
 
     vocals_path, music_path = separate_tracks(audio_path)
 
-    print(f"Creating karaoke video... {vocals_path} {music_path} {video_path}")
+    print(f"Creating karaoke video..")
 
     create(vocals_path, music_path, video_path)
 
